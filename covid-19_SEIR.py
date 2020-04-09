c01fc3ffedd9aa487195d767ab1c7b325d752706
bitjungle <devel@bitjungle.com>
2020-04-09 08:08:05 +0200

    better handling of variable R0

--- covid-19_SEIR.py
+++ covid-19_SEIR.py
@@ -25,7 +25,7 @@ E_start = I_start * 10 # Number of infected at simulation start
 R_start = 0
 R0_start =  2.54      # basic reproduction number at simulation start
 R0 =  R0_start       # basic reproduction number
-gamma = 1 / 10       # 1 / duration of infectiousness
+gamma = 1 / 14       # 1 / duration of infectiousness
 sigma = 1/3.5 # the infection rate calculated by the inverse of the mean latent period
 beta = R0 * gamma
 fr = 0.0138 # Fatality ratio 1,38 % 
@@ -33,18 +33,18 @@ fr = 0.0138 # Fatality ratio 1,38 %
 # Significant dates and R0 changes -------------------------------------
 date_start = date(2020,2,24) # Monday after winter holiday in Norway
 # Significant government action dates 
-date_gov_actions = (date(2020,3,12),  date(2020,3,20), date(2020,3,27))
+use_gov_actions = True
+date_gov_actions = (date(2020,3,12),  date(2020,3,27), date(2020,6,15))
 date_gov_actions_days = ((date_gov_actions[0] - date_start).days, 
                          (date_gov_actions[1] - date_start).days,
                          (date_gov_actions[2] - date_start).days)
 date_today = date.today()
 date_delta = (date_today - date_start).days
-use_gov_actions = True
 # basic reproduction number after gov. actions
-R0_gov_action =  (0.71, 0.9, 1.0)
+R0_gov_action =  (0.71, 1.0, 1.3)
 
 # Time horizon and time step -------------------------------------------
-t_max = 365 # number of days to simulate
+t_max = 365*2 # number of days to simulate
 dt = 1      # time step in days
 num_iter = math.ceil(t_max/dt) # number of iterations in simulation
 
@@ -64,10 +64,12 @@ model = seir.SEIR_model(S[0], E[0], I[0], R[0], beta, gamma, sigma, N)
 
 # Simulation -----------------------------------------------------------
 for i in range(1, num_iter):
-    if use_gov_actions and i == date_gov_actions_days[0]: 
-        print("Changing R0 to {} after {} days".format(R0_gov_action, i))
-        beta = R0_gov_action[0] * gamma
-        model.set_beta(beta)
+    if use_gov_actions:
+        if i in date_gov_actions_days: 
+            idx = date_gov_actions_days.index(i)
+            print("Changing R0 to {} after {} days".format(R0_gov_action[idx], i))
+            beta = R0_gov_action[idx] * gamma
+            model.set_beta(beta)
     model.update(dt)
     S[i] = model.get_S()
     E[i] = model.get_E()
@@ -83,13 +85,17 @@ print("Final numbers:")
 print("Susceptible: {:.0f} - Exposed: {:.0f} - Infectious: {:.0f} - Recovered: {:.0f} - Fatalities {:.0f}"
       .format(S[-1], E[-1], I[-1], R[-1], F[-1]))
 
-plt.title("Spread of corona virus (SEIR model), $N={:5.0f}$ \n $\\beta={:5.2f}$ $\\gamma={:5.2f}$ $\\sigma={:5.2f}$ $R_0={:5.2f}\\rightarrow{:5.2f}$"
-          .format(N, model.get_beta(), gamma, sigma, R0_start, R0_gov_action[0]))
+plt_title = "Spread of corona virus (SEIR model), $N={:5.0f}$\nR0 ".format(N)
+for r in R0_gov_action:
+    plt_title += '→ ' + str(r)
+
+plt.title(plt_title)
 plt.plot(S, label='Susceptible')
 plt.plot(E, label='Exposed')
 plt.plot(I, label='Infectious')
 plt.plot(R, label='Removed')
-plt.axvline(date_gov_actions_days[0], color='magenta', linestyle='--')
+for d in date_gov_actions_days:
+    plt.axvline(d, color='magenta', linestyle='--')
 plt.grid()
 plt.xlabel('Days')
 plt.ylabel('Number of people')
