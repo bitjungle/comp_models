640efd95f4f46be1b9b2a67797ed6ca2bc69df6d
Rune Mathisen <devel@bitjungle.com>
2022-02-23 10:41:47 +0100

    test updates

--- test_sir_model.py
+++ test_sir_model.py
@@ -1,35 +1,58 @@
+from tkinter import mainloop
 from sir_model import SIR_model
 import unittest 
 
 class TestSIR(unittest.TestCase):
+    '''Unit tests for the SIR model class'''
+
+    @classmethod
+    def setUpClass(cls):
+        '''A class method called before tests in an individual class are run'''
+        pass
+    
+    @classmethod
+    def tearDownClass(cls):
+        '''A class method called after tests in an individual class have run'''
+        pass
+    
+    def setUp(self):
+        '''Method called to prepare the test fixture'''
+        self.S_0 = 95
+        self.I_0 = 5
+        self.R_0 = 0
+        self.N = self.S_0 + self.I_0 + self.R_0
+        self.gamma = 1/10
+        self.beta = 1/3
+        self.dt = 1
+        
+        self.model = SIR_model(self.S_0, self.I_0, self.R_0,
+                               self.beta, self.gamma)
+    
+    def tearDown(self):
+        '''Method called immediately after the test method has been called'''
+        pass
     
     def test_can_construct(self) -> None:
-        S_0 = 95
-        I_0 = 5
-        R_0 = 0
-        gamma = 1/14
-        beta = 1/3
-        model = SIR_model(S_0, I_0, R_0, beta, gamma)        
-
-        self.assertAlmostEqual(model.S, S_0)
-        self.assertAlmostEqual(model.I, I_0)
-        self.assertAlmostEqual(model.R, R_0)
+        self.assertAlmostEqual(self.model.S, self.S_0)
+        self.assertAlmostEqual(self.model.I, self.I_0)
+        self.assertAlmostEqual(self.model.R, self.R_0)
 
     def test_update_gives_expected_trends(self) -> None:
-        S_0 = 95
-        I_0 = 5
-        R_0 = 0
-        N = S_0 + I_0 + R_0
-        gamma = 1/14
-        beta = 1/3
-        dt = 1
-        model = SIR_model(S_0, I_0, R_0, beta, gamma)        
         for i in range(1, 25):
-            S_old = model.S
-            I_old = model.I
-            R_old = model.R
-            model.update(dt)
+            self.S_old = self.model.S
+            self.I_old = self.model.I
+            self.R_old = self.model.R
+            self.model.update(self.dt)
 
-            self.assertLessEqual(model.S, S_old)
-            self.assertAlmostEqual(N/100, (model.S + model.I + model.R)/100, places=0)
-            self.assertGreaterEqual(model.R, R_old)
+            self.assertLessEqual(self.model.S, self.S_old)
+            self.assertAlmostEqual(self.N/100, 
+                                   (self.model.S + self.model.I + self.model.R)/100,
+                                   places=0)
+            self.assertGreaterEqual(self.model.R, self.R_old)
+
+    def test_R0_is_calculated_correctly(self) -> None:
+        self.assertEqual(self.model.R0, self.beta/self.gamma)
+
+
+if __name__ == "__main__":
+    unittest.main()
