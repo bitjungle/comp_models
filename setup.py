5b51a18ec29702720602b07864ec6135a65c545c
Rune Mathisen <devel@bitjungle.com>
2022-03-16 17:33:57 +0100

    setup updates

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="comp_models",
-    version="0.1.0",
+    name='comp_models',
+    description='Implementation of some models for compartmental models in epidemiology',
+    version='0.1.0',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
     packages=find_packages(include=['comp_models', 'comp_models.*'])
 )
